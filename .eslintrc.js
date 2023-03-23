54fbc4336a5d533af8a24908f4b0d39d2e41abe8
Daniel Ruiz <danielruimar@gmail.com>
2023-03-23 21:34:48 +0100

    add slider component

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    semi: 'off',
+    'react/react-in-jsx-scope': 'off',
+    'space-before-function-paren': 'off',
+    'react/jsx-indent': [2, 2],
+    'react/prop-types': 'off',
+    'jsx-quotes': [2, 'prefer-single'],
+    'multiline-ternary': 'off',
+    'react/no-unescaped-entities': 'off',
+    'comma-dangle': 'off'
+  }
+}
